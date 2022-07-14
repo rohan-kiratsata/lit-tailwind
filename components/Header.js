85afdd395dcb22eba6d506d969f72175b111ff5e
Rohan Kiratsata <49232485+rohan-kiratsata@users.noreply.github.com>
2022-07-14 11:42:56 +0530

    project init

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className="text-gray-600 body-font bg-red-200">
+            <div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+
+                    <span className="text-xl">Tailblocks</span>
+                </a>
+                <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                    <a className="mr-5 hover:text-gray-900">First Link</a>
+                    <a className="mr-5 hover:text-gray-900">Second Link</a>
+                    <a className="mr-5 hover:text-gray-900">Third Link</a>
+                    <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+                </nav>
+                <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button</button>
+            </div>
+        </header>
+    )
+}
